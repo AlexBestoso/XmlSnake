468ac1b6fb53042b865c06dbab500c10e67a0a2d
Morning Star <alexbestoso@gmail.com>
2023-04-10 18:52:12 -0400

    Removed comment out test functions

--- xmlSnake.class.h
+++ xmlSnake.class.h
@@ -85,32 +85,6 @@ class XmlSnake{
 	/*
 	 * Reader functions
 	 * */
-
-	/*void processNode(xmlTextReaderPtr reader) {
-    		const xmlChar *name, *value;
-
-    		name = xmlTextReaderConstName(reader);
-    		if (name == NULL)
-			name = BAD_CAST "--";
-		
-		    value = xmlTextReaderConstValue(reader);
-		
-		    printf("%d %d %s %d %d",
-			    xmlTextReaderDepth(reader),
-			    xmlTextReaderNodeType(reader),
-			    name,
-			    xmlTextReaderIsEmptyElement(reader),
-			    xmlTextReaderHasValue(reader));
-		    if (value == NULL)
-			printf("\n");
-		    else {
-		        if (xmlStrlen(value) > 40)
-		            printf(" %.40s...\n", value);
-		        else
-			    printf(" %s\n", value);
-		    }
-	}*/
-
 	bool openFileReader(string fileName){
 		reader = xmlReaderForFile(fileName.c_str(), NULL, 0);
 		if(reader == NULL){
@@ -173,26 +147,6 @@ class XmlSnake{
 			xmlCleanupParser();
 		}
 	}
-	/*void streamFile(const char *filename) {
-	    	//xmlTextReaderPtr reader;
-	    	int ret;
-	
-    		reader = xmlReaderForFile(filename, NULL, 0);
-    		if (reader != NULL) {
-    		    ret = xmlTextReaderRead(reader);
-    		    while (ret == 1) {
-    		        processNode(reader);
-    		        ret = xmlTextReaderRead(reader);
-    		    }
-    		    xmlFreeTextReader(reader);
-    		    if (ret != 0) {
-    		        fprintf(stderr, "%s : failed to parse\n", filename);
-    		    }
-    		} else {
-    		    fprintf(stderr, "Unable to open %s\n", filename);
-    		}
-	}*/
-
 	void cleanReader(){
 		xmlCleanupParser();
 	}
